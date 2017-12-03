378a6440aca422831590b909f5661f2fada533fc
Renbo <rt2632@columbia.edu>
2017-12-02 21:43:09 -0500

    csv

--- scripts/utils/read_csv.py
+++ scripts/utils/read_csv.py
@@ -1,5 +1,8 @@
 #import csv
+import sys
 import pandas as pd
+
+
 
 def read_barred(file):
 	'''this function returns a list of names of interest, 
@@ -11,14 +14,10 @@ def read_barred(file):
 			#names.append(names[1])
 	#return names
 	df = pd.read_csv(file)
+	#print('executed')
 	
 	return df
 
-
-
-
-
-
-
-
-	
+#if __name__ == "__main__":
+	#print('executing')
+	#print(read_barred(str(sys.argv[1]))
